f905905956855ed15e937cd2a6af30f1266b6036
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-07 12:46:26 +0200

    Create dnsfwzones.sh

---
+++ dnsfwzones.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+export listenerip="a.b.c.d"
+complist=$(oci iam compartment list --all --compartment-id-in-subtree true) 
+complistcur=$(echo $complist | jq .data | jq -r '.[] | ."id"')
+rm -f zonelist.log
+rm -f named.conf
+rm -f forwardwlist.named
+
+cat stdnamedconf.ref > named.conf
+for compocid in $complistcur; do oci dns zone list --compartment-id $compocid --all --scope PRIVATE | jq .data | jq -r '.[] | ."name"' >> zonelist.log ; done
+zones=$(cat zonelist.log) 
+for zone in $zones; do ./namedconfbuild.sh $zone ; done 
+sed -i 's/169.254.169.254/'$listenerip'/g' forwardwlist.named
+cat forwardwlist.named >> named.conf
+
+#\cp -f named.conf /etc/named.conf
