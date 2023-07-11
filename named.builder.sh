693919fd7da46387ca1b9ab5006fdf99cd5f431b
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-07-11 19:45:31 +0200

    Create named.builder.sh

---
+++ named.builder.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#named.builder.sh 
+echo "zone "$1" {" >> forwardwlist.named
+echo "        type forward;" >> forwardwlist.named
+echo "        forwarders { 169.254.169.254; };" >> forwardwlist.named
+echo "};" >> forwardwlist.json
